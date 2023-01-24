a31bf01fcf7fbab99dcc3c017a4b18fa92c362ce
Connor Carciofini <ccarciofini@gmail.com>
2023-01-23 22:52:36 -0600

    thats about it

--- script.py
+++ script.py
@@ -1,18 +1,29 @@
 from pytube import YouTube
 import os
+import sys
 import pyfiglet
+    
+def intro():
+    result = pyfiglet.figlet_format("PyTunes", font = "isometric3" )
+    print(result)
+    print('SHLERM INDUSTRIAL SOLUTIONS CORP')
 
-result = pyfiglet.figlet_format("PyTunes", font = "isometric3" )
-print(result)
-print('SHLERMS INDUSTRIAL SOLUTIONS CORP')
+def main(): 
+    url = input('AWAITING URL...("EXIT" TO TERMINATE)\n')
+    if url.lower() == 'exit':
+        print('SIYANORA!')
+        sys.exit()
 
-yt = YouTube(str(input('AWAITING URL...\n')))
+    yt = YouTube(str(url))
+    extract = yt.streams.filter(only_audio = True).first()
+    out_file = extract.download(output_path = './tunage')
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
 
-extract = yt.streams.filter(only_audio = True).first()
-out_file = extract.download(output_path = './tunage')
-base, ext = os.path.splitext(out_file)
-new_file = base + '.mp3'
+    print(yt.title + ' DOWNLOADED SUCCESSFULLY')
+    print('GROOVE ON SOUL BROTHER')
+    main()
 
-os.rename(out_file, new_file)
-
-print(yt.title + ' DOWNLOADED SUCCESSFULLY')
+intro()
+main()
