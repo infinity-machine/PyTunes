947cbb5430ff7d9d48b48b34dfef6bf49a6fa006
Connor Carciofini <ccarciofini@gmail.com>
2023-01-23 21:38:44 -0600

    init

---
+++ script.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+import os
+
+yt = YouTube(str(input('ENTER URL...\n')))
+
+extract = yt.streams.filter(only_audio = True).first()
+
+out_file = extract.download(output_path = '/Users/connor/Desktop/PyTunes/tunage')
+
+base, ext = os.path.splitext(out_file)
+
+new_file = base + '.mp3'
+
+os.rename(out_file, new_file)
+
+print(yt.title + ' DOWNLOADED SUCCESSFULLY')
