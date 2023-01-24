b7d24145ee4923b72a61b263e769cbfcba30538d
Connor Carciofini <ccarciofini@gmail.com>
2023-01-23 22:16:52 -0600

    boutta get fancy

--- script.py
+++ script.py
@@ -1,14 +1,16 @@
 from pytube import YouTube
 import os
+import pyfiglet
 
-yt = YouTube(str(input('ENTER URL...\n')))
+result = pyfiglet.figlet_format("PyTunes", font = "isometric3" )
+print(result)
+print('SHLERMS INDUSTRIAL SOLUTIONS CORP')
+
+yt = YouTube(str(input('AWAITING URL...\n')))
 
 extract = yt.streams.filter(only_audio = True).first()
-
 out_file = extract.download(output_path = './tunage')
-
 base, ext = os.path.splitext(out_file)
-
 new_file = base + '.mp3'
 
 os.rename(out_file, new_file)
